24694bd053e7063298bfb101b20477e2ee54a0d6
Quidvio <105707614+klinbee@users.noreply.github.com>
2024-09-28 16:18:40 -0500

    chore: name/format changes....

---
+++ common/src/main/java/com/klinbee/badapple/Constants.java
@@ -0,0 +1,11 @@
+package com.klinbee.badapple;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "badapple";
+	public static final String MOD_NAME = "Bad Apple World Preset";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/com/klinbee/badapple/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.klinbee.badapple.platform;
+
+import com.klinbee.badapple.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
