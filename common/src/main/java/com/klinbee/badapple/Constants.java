ed0e9c7ae8bce5133c486e1dd0347ca0f67ed9ff
Quidvio <105707614+klinbee@users.noreply.github.com>
2024-09-29 22:32:26 -0500

    change: mod_id

--- common/src/main/java/com/klinbee/badapple/Constants.java
+++ common/src/main/java/com/klinbee/badapple/Constants.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class Constants {
 
-	public static final String MOD_ID = "badapple";
+	public static final String MOD_ID = "bad_apple_world";
 	public static final String MOD_NAME = "Bad Apple World Preset";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
